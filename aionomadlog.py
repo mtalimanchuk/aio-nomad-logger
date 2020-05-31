4bbe04c9d7e7e2e866b6d1cb159186fd456da3a6
Maxim Talimanchuk <mtalimanchuk@gmail.com>
2020-06-01 01:56:41 +0300

    yield logs via .as_completed instead of .gather

--- aionomadlog.py
+++ aionomadlog.py
@@ -20,7 +20,13 @@ from config import NOMAD_HOST, NOMAD_PORT
 
 class NomadTaskList:
     def __init__(
-        self, host: str, port: int, task_name_glob: str, save_path: str = "tasks.json",
+        self,
+        host: str,
+        port: int,
+        task_name_glob: str,
+        save_path: str = "tasks.json",
+        log_delay: int = 1,
+        max_log_length: int = 100000,
     ):
         self.host = host
         self.port = port
@@ -30,13 +36,13 @@ class NomadTaskList:
         self._allocations_url = f"{self._base_url}/allocations"
         self._logs_url = f"{self._base_url}/client/fs/logs"
 
-        self._log_delay = 2
-        self._token_bytes = 4
+        self._token_bytes = 3   # used as random hex color
+        self._log_delay = log_delay
+        self._max_log_length = max_log_length
 
         self._save_path = save_path
 
         self.task_name_glob = task_name_glob
-        print(self.__dict__)
 
     async def __aenter__(self):
         await self.refresh_sources()
@@ -54,7 +60,12 @@ class NomadTaskList:
             for task_name in allocation["TaskStates"].keys():
                 # fnmatch allows matching Unix shell-style wildcards: * ? [seq] [!seq]
                 if fnmatch.fnmatch(name=task_name, pat="Statistics*"):
-                    task_id = token_hex(self._token_bytes)
+
+                    import random
+                    random_digit = random.choice(range(8))
+                    color = f"\u001b[9{random_digit}m"
+
+                    task_id = color
                     alloc_id = allocation["ID"]
                     now = datetime.utcnow()
 
@@ -70,6 +81,7 @@ class NomadTaskList:
 
     async def _fetch_log(self, task_id, alloc_id, name, type, offset) -> dict:
         url = f"{self._logs_url}/{alloc_id}?task={name}&type={type}&offset={offset}"
+        print(f"Fetching {name}")
         response = await self._session.get(url)
         raw_text = await response.text()
 
@@ -88,9 +100,12 @@ class NomadTaskList:
 
         tasks = await self._load_tasks()
         tasks[task_id]["offset"] = offset
+        if not tasks:
+            print("gotcha!")
         await self._save_tasks(tasks)
+        print(f"Fetched and saved {name}")
 
-        return {"text": text, "filename": filename, "offset": offset}
+        return {"task_id": task_id, "text": text, "filename": filename, "offset": offset}
 
     async def _save_tasks(self, tasks: dict) -> None:
         async with aiofiles.open(self._save_path, mode="w") as alloc_f:
@@ -105,35 +120,55 @@ class NomadTaskList:
 
     async def refresh_sources(self) -> None:
         filtered_tasks = await self._fetch_tasks()
+        if not filtered_tasks:
+            print("gotcha2!")
         await self._save_tasks(filtered_tasks)
+
+    def print_log(self, log):
+        if log.get("text"):
+
+            color = log["task_id"]
+            color_stop = "\u001b[0m"
+
+            text = log["text"]
+            filename = log["filename"]
+            offset = log['offset']
+            delimiter = '=' * 20
+
+            if len(text) > self._max_log_length:
+                text = "... too long to print ..."
+
+            print(
+                f"{color}{delimiter} {filename} @offset {offset} {delimiter}\n{text}{color_stop}"
+            )
 
     async def stream_batch(self):
         tasks = await self._load_tasks()
-        coroutines = (
-            self._fetch_log(task_id, **task_data)
-            for task_id, task_data in tasks.items()
-        )
-        log_batch = await asyncio.gather(*coroutines)
-        return log_batch
+        aiotasks = []
+        for task_id, task_data in tasks.items():
+            coroutine = self._fetch_log(task_id, **task_data)
+            aiotask = asyncio.create_task(coroutine)
+            aiotasks.append(aiotask)
+
+        for completed_task in asyncio.as_completed(aiotasks):
+            completed_task_data = await completed_task
+            self.print_log(completed_task_data)
+
+    async def idle(self):
+        await asyncio.sleep(self._log_delay)
 
 
 async def main():
     do_allocation_refresh = True
     async with NomadTaskList(NOMAD_HOST, NOMAD_PORT, "Statistics*") as logger:
         while True:
-            print("New batch")
+            print(f"\n{'*' * 100}\n")
             if do_allocation_refresh:
                 await logger.refresh_sources()
                 do_allocation_refresh = False
             else:
-                batch = await logger.stream_batch()
-                for log in batch:
-                    if len(log["text"]) > 10000:
-                        log["text"] = "Too long to print"
-                    print(
-                        f"{log['filename']} @offset {log['offset']} {'=' * 30}\n{log['text']}"
-                    )
-            await asyncio.sleep(5)
+                await logger.stream_batch()
+                await logger.idle()
 
 
 if __name__ == "__main__":
